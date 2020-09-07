3c0e1373ea944c1cf6861433b2cf7d0d8d79f26f
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-09-07 21:50:59 +0900

    I studied print, dump and let, var

---
+++ 02_let_var.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+// 상수 선언 키워드 let : 차후에 변경이 불가능한 변수
+// 변수 선언 키워드 var : 차후에 변경이 가능한 변수
+
+let name: String = "Thomas"
+var age: Int = 24
+
+// 변수의 타입이 값을 통해 유추가 가능할 경우 생략 가능
+
+let name_ = "JunHyeong"
+var age_ = 24
+
+// 나중에 값을 할당하기 위해서는 변수의 타입 명시가 필수적이다.
+let Sum: Int
+let InputA: Int = 240
+let InputB: Int = 120
+
+Sum = InputA + InputB // let 이기 때문에 처음 한번만 할당이 가능
